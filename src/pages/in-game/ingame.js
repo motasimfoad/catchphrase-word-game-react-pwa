f08ab91323535fc83be8ad9013d5028e6e5cd8e8
Motasim Foad <motasimfoad@gmail.com>
2020-12-31 05:52:24 +0600

    Most of the game without sounds

--- src/pages/in-game/ingame.js
+++ src/pages/in-game/ingame.js
@@ -5,41 +5,43 @@ import {useLocation, Link} from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faLowVision, faEye } from '@fortawesome/free-solid-svg-icons';
 
+
 const year = new Date();
 
-
-const myObj = [ "Ford", "BMW", "Fiat" ] 
-  
-
 function Ingame() {
+  const [myObj, setMyObj] = useState(["a","b","c"]);
   const { state } = useLocation();
   const [min, setMin] = useState(state);
   const [millisec, setMillisec] = useState((min*60)*1000);
   const [seconds, setSeconds] = useState(0);
   const [secToMilli, setSecToMilli] = useState((min*60)*1000);
   const [flag, setFlag] = useState(true);
-  console.log("secons" + secToMilli + " millisec" + millisec+ " flag/true" +flag);
-  const [rand, setRand] =  useState(Math.random() * 2);
-  const [temp, setTemp] = useState(Math.floor(rand));
+  const [rand, setRand] =  useState(Math.round(Math.random() * (myObj.length-1)));
+  const [temp, setTemp] = useState(rand);
+  
+  const getData=()=>{
+    fetch('http://api.motasimfoad.com/catch_phrase/catch_phrase.json'
+    ).then(function(response){
+        return response.json();
+      })
+      .then(function(myJson) {
+        setMyObj(myJson);
+      });
+  }
 
   function toggle() {
     setFlag(!flag);
   }
 
   function word() {
-   
-    if (rand == temp) {
-      word();
-    } else {
-      setTemp ( Math.floor(rand));
-      console.log(Math.floor(rand));
-    }
-    
-    
+    setRand(Math.round(Math.random() * (myObj.length-1)));
+    console.log("temp "+ temp+ " random "+ rand);
+      setTemp ( Math.round(rand));
+      console.log(Math.round(rand));
   }
 
   useEffect(() => {
-
+    getData();
      const interval = setInterval(() => {
           setSeconds(seconds => seconds + 1);
           setSecToMilli(seconds*1000);
@@ -54,48 +56,45 @@ function Ingame() {
     
     }, []);
  
-
-
-
-  return (
-    <Container className="App" fluid>
-        <Row>
-        <Alert className="App-Header" variant="light">
-          <h1>CatchPhrase</h1>
-          <small>- Word/Party Game</small>
-        </Alert>
-        </Row>
-        <Row>
-          <Col>
-            <Alert className="App-Header" variant="dark">
-              {flag ? <h3>{seconds}</h3>: <h4>hidden</h4>}
-              <small>Timer</small>
+    return (
+        <Container className="App" fluid>
+            <Row>
+            <Alert className="App-Header" variant="light">
+              <h1>CatchPhrase</h1>
+              <small>- Word/Party Game</small>
             </Alert>
-          </Col>
-          <Col>
-            <Alert className="App-Header" variant="dark">
-              <Button variant="outline-dark" onClick={toggle}>{flag ? <FontAwesomeIcon icon={faLowVision} />: <FontAwesomeIcon icon={faEye} />}</Button>
-              <small>Visibility</small>
-            </Alert>
-          </Col>
-        </Row>
-        <Row>
-        <Card>
-          <Card.Body className="App-Game-Rules">
-           {myObj[0]}
-          </Card.Body>
-          <Card.Footer>
-            <ButtonGroup aria-label="Basic example">
-              <Button variant="warning" className="btnPadding" onClick={word}>Next</Button>
-              <Link to="/pregame"><Button variant="danger" className="btnPadding">Reset</Button></Link>
-            </ButtonGroup>   
-          </Card.Footer>
-        </Card>
-        </Row>
-        <Row>
-        &copy; {year.getFullYear()}<a href="https://motasimfoad.com" target="_blank" rel="noreferrer">&nbsp; Motasim Foad</a>
-        </Row>
-    </Container>
+            </Row>
+            <Row>
+              <Col>
+                <Alert className="App-Header" variant="dark">
+                  {flag ? <h3>{seconds}</h3>: <h4>hidden</h4>}
+                  <small>Timer</small>
+                </Alert>
+              </Col>
+              <Col>
+                <Alert className="App-Header" variant="dark">
+                  <Button variant="outline-dark" onClick={toggle}>{flag ? <FontAwesomeIcon icon={faLowVision} />: <FontAwesomeIcon icon={faEye} />}</Button>
+                  <small>Visibility</small>
+                </Alert>
+              </Col>
+            </Row>
+            <Row>
+            <Card>
+              <Card.Body className="App-Game-Rules">
+              {myObj[temp]}
+              </Card.Body>
+              <Card.Footer>
+                <ButtonGroup aria-label="Basic example">
+                  <Button variant="warning" className="btnPadding" onClick={word}>Next</Button>
+                  <Link to="/pregame"><Button variant="danger" className="btnPadding">Reset</Button></Link>
+                </ButtonGroup>   
+              </Card.Footer>
+            </Card>
+            </Row>
+            <Row>
+            &copy; {year.getFullYear()}<a href="https://motasimfoad.com" target="_blank" rel="noreferrer">&nbsp; Motasim Foad</a>
+            </Row>
+        </Container>
   );
 }
 
